55c7dd652c1e347cf25ab3ee61932a3b4e26ca7e
Tosh Roberts Brockway <tosh.robertsbrockway@gmail.com>
2021-11-25 10:10:25 -0800

    Exercise 23 and testing it

--- file_overlap_23.py
+++ file_overlap_23.py
@@ -4,9 +4,15 @@ def convert_to_set_of_ints(file):
     return result
 
 
-with open("primenumbers.txt") as file1, open("happynumbers.txt") as file2:
-    file1_converted = convert_to_set_of_ints(file1)
-    file2_converted = convert_to_set_of_ints(file2)
-    result = file1_converted.intersection(file2_converted)
+def intersection_of_two_files(convert_to_set_of_ints, file1_name, file2_name):
+    with open(file1_name) as file1, open(file2_name) as file2:
+        file1_converted = convert_to_set_of_ints(file1)
+        file2_converted = convert_to_set_of_ints(file2)
+        result = file1_converted.intersection(file2_converted)
+    return result
+
+
+result = intersection_of_two_files(
+    convert_to_set_of_ints, file1_name="primenumbers.txt", file2_name="happynumbers.txt")
 
 print(result)
